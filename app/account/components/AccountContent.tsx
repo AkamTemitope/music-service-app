86b2546ac6b2f6fede2863463847ce3eaa3539dd
Akamtemitope <temitopeakam@gmail.com>
2023-05-21 00:27:24 +0300

    feat: add  account, liked fxn

---
+++ app/account/components/AccountContent.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { useUser } from "@/hooks/useUser";
+import Button from "@/components/Button";
+import useSubscribeModal from "@/hooks/useSubscribeModal";
+import { postData } from "@/libs/helpers";
+
+const AccountContent = () => {
+  const router = useRouter();
+  const subscribeModal = useSubscribeModal();
+  const { isLoading, subscription, user } = useUser();
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace('/');
+    }
+  }, [isLoading, user, router]);
+
+  const redirectToCustomerPortal = async () => {
+    setLoading(true);
+    try {
+      const { url, error } = await postData({
+        url: '/api/create-portal-link'
+      });
+      window.location.assign(url);
+    } catch (error) {
+      if (error) return alert((error as Error).message);
+    }
+    setLoading(false);
+  };
+
+  return ( 
+    <div className="mb-7 px-6">
+      {!subscription && (
+        <div className="flex flex-col gap-y-4">
+        <h1>{user?.email}</h1>
+        <p>No active plan.</p>
+        <Button 
+          onClick={subscribeModal.onOpen}
+          className="w-[300px]"
+        >
+          Subscribe
+        </Button>
+      </div>
+      )}
+      {subscription && (
+        <div className="flex flex-col gap-y-4">
+          <h1>{user?.email}</h1>
+          <p>You are currently on the 
+            <b className="text-blue-400"> {subscription?.prices?.products?.name} </b> 
+            plan.
+          </p>
+          <Button
+            disabled={loading || isLoading}
+            onClick={redirectToCustomerPortal}
+            className="w-[300px]"
+          >
+            Open Customer Portal
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default AccountContent;
